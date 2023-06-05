e424dedeb9ced00be145331de3a91da5f4b3a879
Ashmit k <ashmithkacheri138@gmail.com>
2023-06-05 17:12:16 +0530

    Added skills section Added toolstip

--- script.js
+++ script.js
@@ -17,17 +17,44 @@ var typed = new Typed(".typing",{
     loop:true
 });
 
+// tooltips
 
-// const aboutMe = document.getElementById('about-me');
+tippy('#html', {
+  content: 'HTML or The HyperText Markup Language is the standard markup language for documents designed to be displayed in a web browser.',
+  animation: 'fade',
+});
+tippy('#css', {
+  content: 'CSS, or Cascading Style Sheets, is a style sheet language used to describe the visual presentation of HTML and XML documents, allowing developers to control the layout, design, and appearance of web pages.',
+  animation: 'fade'
+});
+tippy('#js', {
+  content: 'JavaScript is a versatile programming language that enables dynamic behavior and interactivity on web pages, making it an essential tool for web development.',
+  animation: 'fade'
+});
+tippy('#bootstrap', {
+  content: 'Bootstrap is an open-source framework that simplifies the process of designing and developing responsive websites and web applications with pre-built CSS and JavaScript components.',
+  animation: 'fade'
+});
+tippy('#github', {
+  content: 'GitHub is a web-based platform that enables version control, collaboration, and hosting of code repositories, making it a central hub for developers to manage and share their projects.',
+  animation: 'fade'
+});
+tippy('#jquery', {
+  content: 'jQuery is a fast and concise JavaScript library that simplifies HTML document traversal, event handling, and animation, making it easier to build interactive and dynamic web pages.',
+  animation: 'fade'
+});
+tippy('#react', {
+  content: 'React is a powerful JavaScript library for building user interfaces that allows developers to create reusable UI components and efficiently update and render data-driven views.',
+  animation: 'fade'
+});
+tippy('#sass', {
+  content: 'Sass is a CSS preprocessor that enhances the capabilities of CSS by providing features like variables, mixins, nesting, and inheritance, simplifying the process of writing and maintaining CSS code.',
+  animation: 'fade'
+});
 
-// window.addEventListener('scroll', () => {
-//   const scrollPosition = window.scrollY;
-//   if (scrollPosition > 500) {
-//     aboutMe.classList.add('about-me-visible');
-//   } else {
-//     aboutMe.classList.remove('about-me-visible');
-//   }
-// });
+
+
+
 
 
 
